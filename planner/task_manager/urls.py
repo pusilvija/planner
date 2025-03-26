afc79d10f55cea1e8174e699760b43790f095a2a
pusilvija <silvijapupsaite@gmail.com>
2025-03-26 11:28:28 +0200

    fix: remove unneccesary update url

--- planner/task_manager/urls.py
+++ planner/task_manager/urls.py
@@ -9,5 +9,4 @@ urlpatterns = [
     path("add/", views.add_task, name='add_task'),
     path("tasks/delete/<int:task_id>/", views.delete_task, name="delete_task"),
     path('task/<int:task_id>/', views.task_details, name='task_details'),
-    path('update/<int:task_id>/', views.update_task, name='update_task'),
 ]
